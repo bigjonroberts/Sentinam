bd9b8e39b3e2ee37274b8205054c7a425e582453
Jon Roberts <bigjonroberts@gmail.com>
2021-03-22 23:28:40 -0500

    Bump version to 0.6.0
    ## [0.6.0] - 2021-03-22
    
    ### Added
    - Add SqlStreamStore Persistence (https://github.com/bigjonroberts/Sentinam/pull/6)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to Sentinam will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## Unreleased
+## [0.6.0] - 2021-03-22
 
 ### Added
 - Add SqlStreamStore Persistence (https://github.com/bigjonroberts/Sentinam/pull/6)
@@ -26,6 +26,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - This release contains core functionality
 - Types and functions to easily generate Agents for processing common requests
 - API is still very rough at this stage
-[Unreleased]: https://github.com/bigjonroberts/Sentinam/compare/v0.5.1...HEAD
+[Unreleased]: https://github.com/bigjonroberts/Sentinam/compare/v0.6.0...HEAD
+[0.6.0]: https://github.com/bigjonroberts/Sentinam/compare/v0.5.2...v0.6.0
 [0.5.1]: https://github.com/bigjonroberts/Sentinam/compare/v0.5.0...v0.5.1
 [0.5.0]: https://github.com/bigjonroberts/Sentinam/releases/tag/v0.5.0
--- src/Sentinam.Persistence.Memory/AssemblyInfo.fs
+++ src/Sentinam.Persistence.Memory/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Sentinam.Persistence.Memory")>]
 [<assembly: AssemblyProductAttribute("Sentinam")>]
-[<assembly: AssemblyVersionAttribute("0.5.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-05-09T00:00:00.0000000-05:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.5.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.5.1")>]
+[<assembly: AssemblyVersionAttribute("0.6.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-03-22T00:00:00.0000000-05:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.6.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.6.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","6a9720153ff4967a1c49feaaf5040f1dd7aef009")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","7a2dac6f12a3a794f277edcf1c9e820f54535591")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Sentinam.Persistence.Memory"
     let [<Literal>] AssemblyProduct = "Sentinam"
-    let [<Literal>] AssemblyVersion = "0.5.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-05-09T00:00:00.0000000-05:00"
-    let [<Literal>] AssemblyFileVersion = "0.5.1"
-    let [<Literal>] AssemblyInformationalVersion = "0.5.1"
+    let [<Literal>] AssemblyVersion = "0.6.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-03-22T00:00:00.0000000-05:00"
+    let [<Literal>] AssemblyFileVersion = "0.6.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.6.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "6a9720153ff4967a1c49feaaf5040f1dd7aef009"
+    let [<Literal>] AssemblyMetadata_GitHash = "7a2dac6f12a3a794f277edcf1c9e820f54535591"
